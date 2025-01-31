8699b99d1b8b8332184d1ffafbe5a1f05b73dde0
mperumal-usd <mperumal@sandiego.edu>
2025-01-31 09:12:53 -0800

    title

--- index.md
+++ index.md
@@ -1,15 +1,8 @@
 ---
 title: உலகத் தமிழ்க்கல்விக் கழகம் 
 ---
-<script src="{{ site.baseurl }}/scripts/track.js"></script>
+<script src="{{ site.baseurl }}/scripts/track.js">track()</script>
 <script  src="{{ site.baseurl }}/scripts/main.js"></script>
-### வாரம் 20  
-
-#### உரையாடல் பயிற்சி :  மாதங்கள் 
-#### கதை சொல்லுதல் :  புறாவும் எறும்பும் 
-
-#### Due Date: Feb, 02 2025
 
 <div id="mainPage">
 </div>
-
--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,12 @@
-const tokenValid=sessionStorage.getItem("sessionToken");
+window.addEventListener("load", (event) => {
+    const tokenValid=sessionStorage.getItem("sessionToken");
 if(tokenValid){
     const mainPage=document.getElementById("mainPage");
     if(mainPage){
-        mainPage.innerHTML=`### வாரம் 20  #### உரையாடல் பயிற்சி :  மாதங்கள் 
-#### கதை சொல்லுதல் :  புறாவும் எறும்பும் 
-
-#### Due Date: Feb, 02 2025
+        mainPage.innerHTML=`<p> <h2>வாரம் 20</h2> </br> <h3> உரையாடல் பயிற்சி :  மாதங்கள் </h3>
+<h3> கதை சொல்லுதல் :  புறாவும் எறும்பும் </h3>
+<h3> Due Date: Feb, 02 2025 </h3>
 `
     }
 }
+  });
