82f551b8d6bc6cf676cf8a7a008e388da1a3cf7e
mperumal-usd <mperumal@sandiego.edu>
2025-02-02 22:06:06 -0800

    report changes

--- scripts/class_report.js
+++ scripts/class_report.js
@@ -1,9 +1,3 @@
-const jsonData = [
-    { "id": "User_1", "score": 10, "comments": "", "status": "in progress", "completionDate": "", "dueDate": "2021-08-30", "week": "20", "assignmentType": "", "assignmentId": "" },
-    { "id": "User_1", "score": 10, "comments": "", "status": "in progress", "completionDate": "", "dueDate": "2021-08-30", "week": "20", "assignmentType": "", "assignmentId": "" },
-    { "id": "User_2", "score": 10, "comments": "Good job", "status": "completed", "completionDate": "2025-02-02T06:18:25.397Z", "dueDate": "2021-08-30", "week": "20", "assignmentType": "conversation", "assignmentId": "643d6a05-0a33-4fb6-971e-5dda1b66098d" }
-];
-
 
 async function getClassReport() {
     const query = {
@@ -34,7 +28,7 @@ async function getClassReport() {
 
 window.addEventListener("load",async (event)=>{
     const tableBody = document.querySelector("#jsonTable tbody");
-    jsonData = await getClassReport();
+    const jsonData = await getClassReport();
     jsonData.forEach(item => {
         const row = document.createElement("tr");
         row.innerHTML = `
