90bf81ec4cc63df6e45199e832cd5d62e9c87cde
mperumal-usd <mperumal@sandiego.edu>
2025-02-07 16:47:10 -0800

    week 21

--- scripts/main.js
+++ scripts/main.js
@@ -4,8 +4,7 @@ if(tokenValid){
     const mainPage=document.getElementById("mainPage");
     if(mainPage){
         mainPage.innerHTML=`<p> <h2>வாரம் 20</h2> </br> <h3> உரையாடல் பயிற்சி :  விழாக்கள்‌ </h3>
-                <h3> கேட்டல்‌ கருத்தறிதல்‌: 'ஆயிரம்‌ நாயணங்கள்‌' கதையை கீழே உள்ள உள்ள youtube link ல்‌ கேட்கவும்‌. </h3>
-                <a href="https://www.youtube.com/watch?v=Mt7ah_uU03M">ஆயிரம்‌ நாயணங்கள்</a>
+                <h3> கேட்டல்‌ கருத்தறிதல்‌: 'ஆயிரம்‌ நாணயங்கள்‌'  </h3>
         <h3> Due Date: Feb, 09 2025 </h3>
         </br>
         <h3> குழுத்திட்டப்பணி (தேர்வு 6) - குறும்படம்‌: </h3>
