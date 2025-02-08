1fc4dac52febafc695ec2b9e2d1b7a8bba1e0515
mperumal-usd <mperumal@sandiego.edu>
2025-02-07 16:52:09 -0800

    week 21

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,7 @@ window.addEventListener("load", (event) => {
 if(tokenValid){
     const mainPage=document.getElementById("mainPage");
     if(mainPage){
-        mainPage.innerHTML=`<p> <h2>வாரம் 20</h2> </br> <h3> உரையாடல் பயிற்சி :  விழாக்கள்‌ </h3>
+        mainPage.innerHTML=`<p> <h2>வாரம் 21</h2> </br> <h3> உரையாடல் பயிற்சி :  விழாக்கள்‌ </h3>
                 <h3> கேட்டல்‌ கருத்தறிதல்‌: 'ஆயிரம்‌ நாணயங்கள்‌'  </h3>
         <h3> Due Date: Feb, 09 2025 </h3>
         </br>
