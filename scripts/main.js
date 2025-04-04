81466b9cc5b7e223452395a48cf347e4b30d2650
mperumal-usd <mperumal@sandiego.edu>
2025-04-04 11:56:10 -0700

    week 29

--- scripts/main.js
+++ scripts/main.js
@@ -3,9 +3,9 @@ window.addEventListener("load", (event) => {
     if (tokenValid) {
         const mainPage = document.getElementById("mainPage");
         if (mainPage) {
-            mainPage.innerHTML = `<p> <h2>வாரம் 26</h2> </br> <h3> உரையாடல் பயிற்சி : பழங்கள்‌‌ (Week 25)  </h3>
-                <h3> கதை சொல்லுதல் பயிற்சி: 25 </h3>
-        <h3> Due Date: March, 23 2025 </h3>
+            mainPage.innerHTML = `<p> <h2>வாரம் 29</h2> </br> <h3> உரையாடல் பயிற்சி : உழவர்‌ சந்தை (Week 29)  </h3>
+                <h3> கதை சொல்லுதல் பயிற்சி: 29 </h3>
+        <h3> Due Date: Apr, 6 2025 </h3>
         </br> `
         }
     }
