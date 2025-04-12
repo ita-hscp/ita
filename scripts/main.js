6fb7203f66603dc0192f08283d10cec81470910e
mperumal-usd <mperumal@sandiego.edu>
2025-04-12 09:31:03 -0700

    content creation

--- scripts/main.js
+++ scripts/main.js
@@ -3,9 +3,8 @@ window.addEventListener("load", (event) => {
     if (tokenValid) {
         const mainPage = document.getElementById("mainPage");
         if (mainPage) {
-            mainPage.innerHTML = `<p> <h2>வாரம் 29</h2> </br> <h3> உரையாடல் பயிற்சி : உழவர்‌ சந்தை (Week 29)  </h3>
-                <h3> கதை சொல்லுதல் பயிற்சி: 29 </h3>
-        <h3> Due Date: Apr, 6 2025 </h3>
+            mainPage.innerHTML = `<p> <h2>வாரம் 30</h2> </br> <h3> உரையாடல் பயிற்சி : வீட்டு விலங்குகள் , காட்டு விலங்குகள் (Week 30)  </h3>
+        <h3> Due Date: Apr, 14 2025 </h3>
         </br> `
         }
     }
