d56e9519de47b2e8dd6fb70c86931e38c5832b18
mperumal-usd <mperumal@sandiego.edu>
2025-08-18 21:36:00 -0700

    main

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,47 @@
-window.addEventListener("load", (event) => {
+window.addEventListener("load",async (event) => {
     const tokenValid = sessionStorage.getItem("sessionToken");
     if (tokenValid) {
         const mainPage = document.getElementById("mainPage");
         if (mainPage) {
-                const dateElement = document.getElementById("date");
-                const currentDate = new Date()
-                const options = { 
-                  weekday: 'long', 
-                  year: 'numeric', 
-                  month: 'long', 
-                  day: 'numeric' 
-                };
-            const textContent = currentDate.toLocaleDateString(undefined, options); 
-            mainPage.innerHTML = `<p> <h2>வாரம் 1</h2> </br> 
+            const dateElement = document.getElementById("date");
+            const currentDate = new Date()
+            const options = {
+                weekday: 'long',
+                year: 'numeric',
+                month: 'long',
+                day: 'numeric'
+            };
+            const textContent = currentDate.toLocaleDateString(undefined, options);
+            const mainContent = await mainContent();
+            mainPage.innerHTML = `<p> <h2>${mainContent.title}</h2> </br>
+                <p>${mainContent.content}</p>`
+        }
+    }
+});
+
+async function mainContent() {
+    // const response = await fetch('https://infinite-sands-52519-06605f47cb30.herokuapp.com/mainContent', {
+    //     method: 'GET',
+    //     headers: {
+    //         'Authorization': sessionStorage.getItem('sessionToken'),
+    //         'Content-Type': 'application/json'
+    //     }
+    // });
+    // if (response.status === 401) {
+    //     // Redirect to login page if not authenticated
+    //     window.location.href = "https://ita-hscp.github.io/ita/Login"; // Replace with your actual login URL
+    //     return;
+    // }
+    // if (!response.ok) {
+    //     return {}
+    // }
+    // return response.json();
+    setTimeout(() => {
+    }, 1000); // Simulate a delay for the main content
+    const textContent = currentDate.toLocaleDateString(undefined, options);
+    return {
+        "title": "வாரம் 1",
+        "content": `<p> <h2>வாரம் 1</h2> </br> 
             <h3>முக்கிய அறிவிப்பு</h3>
             <p>இந்த வாரம் நீங்கள் செய்ய வேண்டிய பயிற்சிகள் கீழே உள்ளன:</p>
             <ul>
@@ -24,8 +53,7 @@ window.addEventListener("load", (event) => {
             <h3> இன்று தேதி </h3>
             <div id="date">${textContent}</div> 
             <h3>காலக்கெடு</h3>
-            <p>இந்த பயிற்சிகளை முடிக்க கடைசி தேதி: <span id="dueDate">09-25-2025 </span></p>
-            `;
-        }
-    }
-});
+            <p>இந்த பயிற்சிகளை முடிக்க கடைசி தேதி: <span id="dueDate">09-25-2025 </span></p>`
+    };
+
+}
